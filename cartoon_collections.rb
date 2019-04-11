4fec6b26b7cf12e6506059b3bb10e034d1f83994
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-11 16:51:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+ end 
+
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +18,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+#learn --fail-fast
