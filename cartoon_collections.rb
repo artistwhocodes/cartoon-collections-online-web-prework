42400ccb2d4a0c3233ec410f04169bf909b3e9a6
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-11 16:58:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-   cheese_types
+   cheese = cheese_types
    end
 end
 
